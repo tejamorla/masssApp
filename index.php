b999b2be0a6308c778dec3dabfcc7a581b9ece66
teja <teja85131@gmail.com>
2019-03-07 16:40:06 +0530

    changes

--- index.php
+++ index.php
@@ -29,12 +29,65 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			});
 		</script>
 <!--start-smoth-scrolling-->
+<style>
+			.std_loadingBackground {
+    font-family: Verdana,Arial;
+    filter: Alpha(Opacity=30);
+    -moz-opacity: .3;
+    opacity: .6;
+    width: 100%;
+    height: 1000px;
+    background-color: #000;
+    position: fixed;
+    z-index: 500;
+    top: 0;
+    left: 0;
+
+}
+.std_modalContainer {
+    position: fixed;
+    left: 40%;
+    top: 40%;
+    z-index: 750;
+    background-color: #f2f4f5;
+    padding: 6px;
+    border-radius: 4px;
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+    box-shadow: 5px 5px 5px rgba(0,0,0,.3);
+    /*border: 0 solid #d0d0d0;*/
+     border: 3px solid rgb(170, 0, 0)
+    -webkit-box-shadow: 5px 5px rgba(0,0,0,.3);
+    -moz-box-shadow: 5px 5px rgba(0,0,0,.3);
+
+}
+.std_processing {
+ font-family: Verdana,Arial;
+    text-align: center;color: rgb(0, 0, 0);
+    padding: 15px 15px 0 15px;
+    width: 40%;
+    /*border: 'none',
+    padding: '15px',
+    backgroundColor: '#000',
+    '-webkit-border-radius': '10px', 
+    '-moz-border-radius': '10px',
+    opacity: .5,
+    color: '#fff'
+    /* border: 'none', 
+            padding: '15px', 
+            backgroundColor: '#000', 
+            '-webkit-border-radius': '10px', 
+            '-moz-border-radius': '10px', 
+            opacity: .5, 
+            color: '#fff'*/
+}
+		</style>
 </head>
 <body ng-app="myapp">
 	
 <div ng-include="'views/socialhead.html'"></div>
 <div ng-include="'views/Menu.html'"></div>
-	
+	<div data-loading> </div>
 	<div class="ng-view"></div>
 	<div ng-include="'views/footer.html'"></div>
 </body>
--- js/app.js
+++ js/app.js
@@ -19,5 +19,30 @@ $routeProvider
 	templateUrl:"views/skill-training.html"
 }).when("/community-development",{
 	templateUrl:"views/community-development.html"
+}).when("/contact",{
+	templateUrl:"views/contact.html"
 })
 });
+
+app.directive('loading',   ['$http' ,function ($http)
+ {
+     return {
+         restrict: 'A',
+         template: '<div id="divProcessing"><span class="std_loadingBackground"></span><div style="top: 30%; left: 40%;" class="std_modalContainer"><div class="std_tcenter"><img id="myAnimatedImage" alt="Processing.... Please wait!" src="images/ajax-loader.gif"><span class="std_processing">Processing... Please wait!</span></div></div></div>',
+         link: function (scope, elm, attrs)
+         {
+             scope.isLoading = function () {
+                 return $http.pendingRequests.length > 0;
+             };
+
+             scope.$watch(scope.isLoading, function (v)
+             {
+                 if(v){
+                     elm.show();
+                 }else{
+                     elm.hide();
+                 }
+             });
+         }
+     };
+ }]);
